76524439d2986907e912b67ab8e146e6fef5e777
Christopher Dare <christopher.d.dare@gmail.com>
2017-06-05 12:32:35 +1000

    half way through JUnit testing

--- src/asgn2Tests/CustomerFactoryTests.java
+++ src/asgn2Tests/CustomerFactoryTests.java
@@ -1,5 +1,15 @@
 package asgn2Tests;
+import java.time.LocalTime;
 
+import org.junit.Test;
+
+import asgn2Exceptions.CustomerException;
+
+import asgn2Customers.Customer;
+import asgn2Customers.CustomerFactory;
+import asgn2Customers.DriverDeliveryCustomer;
+import asgn2Customers.PickUpCustomer;
+import asgn2Customers.DroneDeliveryCustomer;
 /**
  * A class the that tests the asgn2Customers.CustomerFactory class.
  * 
@@ -7,5 +17,29 @@ package asgn2Tests;
  *
  */
 public class CustomerFactoryTests {
-	// TO DO
+	
+	// CustomerFactory Exception
+	@Test (expected = CustomerException.class)
+	public void IncorrectCode() throws CustomerException{
+		CustomerFactory.getCustomer("aaa","a","0123456789",5,5);
+	}
+	
+	
+	// Customer Exceptions being passed
+	// This shows that the different exceptions are being passed up as expected
+	
+	@Test (expected = CustomerException.class)
+	public void PhoneGreaterThan10() throws CustomerException{
+		CustomerFactory.getCustomer("PUC","a","01234567899",5,5);
+	}
+	
+	@Test (expected = CustomerException.class)
+	public void YTooLow() throws CustomerException{
+		CustomerFactory.getCustomer("PUC","a","0123456789",5,-50);
+	}
+	
+	@Test (expected = CustomerException.class)
+	public void NameOnlyWhitespace() throws CustomerException{
+		CustomerFactory.getCustomer("PUC","   ","0123456789",5,5);
+	}
 }
--- src/asgn2Tests/CustomerTests.java
+++ src/asgn2Tests/CustomerTests.java
@@ -1,5 +1,15 @@
 package asgn2Tests;
 
+import java.time.LocalTime;
+
+import org.junit.Test;
+
+import asgn2Exceptions.CustomerException;
+
+import asgn2Customers.Customer;
+import asgn2Customers.DriverDeliveryCustomer;
+import asgn2Customers.PickUpCustomer;
+import asgn2Customers.DroneDeliveryCustomer;
 
 /**
  * A class that tests the that tests the asgn2Customers.PickUpCustomer, asgn2Customers.DriverDeliveryCustomer,
@@ -11,5 +21,60 @@ package asgn2Tests;
  *
  */
 public class CustomerTests {
-	// TO DO
+	Customer testCustomer;
+	
+	
+	// Name Exceptions
+	
+	@Test (expected = CustomerException.class)
+	public void NameTooShort() throws CustomerException{
+		testCustomer = new PickUpCustomer("","0123456789",5,5);
+	}
+	
+	@Test (expected = CustomerException.class)
+	public void NameTooLong() throws CustomerException{
+		testCustomer = new PickUpCustomer("aaaaaaaaaaaaaaaaaaaaa","0123456789",5,5);
+	}
+	
+	@Test (expected = CustomerException.class)
+	public void NameOnlyWhitespace() throws CustomerException{
+		testCustomer = new PickUpCustomer("   ","0123456789",5,5);
+	}
+	
+	
+	// Location Exceptions
+	
+	@Test (expected = CustomerException.class)
+	public void XTooHigh() throws CustomerException{
+		testCustomer = new PickUpCustomer("a","0123456789",50,5);
+	}
+	@Test (expected = CustomerException.class)
+	public void XToLow() throws CustomerException{
+		testCustomer = new PickUpCustomer("a","0123456789",-50,5);
+	}
+	@Test (expected = CustomerException.class)
+	public void YTooHigh() throws CustomerException{
+		testCustomer = new PickUpCustomer("a","0123456789",5,50);
+	}
+	
+	@Test (expected = CustomerException.class)
+	public void YTooLow() throws CustomerException{
+		testCustomer = new PickUpCustomer("a","0123456789",5,-50);
+	}
+	
+	
+	// MobileNumber Exceptions
+	
+	@Test (expected = CustomerException.class)
+	public void GreaterThan10() throws CustomerException{
+		testCustomer = new PickUpCustomer("a","01234567899",5,5);
+	}
+	
+	public void LessThan10() throws CustomerException{
+		testCustomer = new PickUpCustomer("a","012345678",5,5);
+	}
+	
+	public void NoLeadingZero() throws CustomerException{
+		testCustomer = new PickUpCustomer("a","1123456789",5,5);
+	}
 }
